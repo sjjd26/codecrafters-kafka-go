0d139b11369e8cc8e5945ab4953ea1a12fc6a192
Stuart Dingwell <stuartdingwell@gmail.com>
2025-09-01 12:45:08 +0100

    check api version

--- app/main.go
+++ app/main.go
@@ -8,6 +8,9 @@ import (
 )
 
 const port = 9092
+const minApiVersionsVersion = int16(0)
+const maxApiVersionsVersion = int16(4)
+const invalidApiVersionErrorCode = uint16(35)
 
 func main() {
 	l, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%v", port))
@@ -28,14 +31,23 @@ func main() {
 		panic(fmt.Sprintf("Error reading input %s", err))
 	}
 
-	// get correlation_id from v2 request header
+	// get correlation_id from v2 request header: https://kafka.apache.org/protocol.html#protocol_messages
 	// 4 bytes for message_size
 	// 2 bytes for request_api_key
-	// 2 bytes for request_api_version
+	// 2 bytes for request_api_version (6:8)
+	requestApiVersion := readBuf[6:8]
+	requestApiVersionInt := int16(binary.BigEndian.Uint16(requestApiVersion))
 	// 4 bytes for correlation_id (8:12)
 	correlationId := readBuf[8:12]
 
+	// Build response
 	response := binary.BigEndian.AppendUint32(nil, uint32(0))
+	response = append(response, correlationId...)
+	if requestApiVersionInt > maxApiVersionsVersion || requestApiVersionInt < minApiVersionsVersion {
+		response = binary.BigEndian.AppendUint16(response, invalidApiVersionErrorCode)
+	} else {
+		response = append(response, 0, 0)
+	}
 	response = append(response, correlationId...)
 
 	_, err = conn.Write(response)
