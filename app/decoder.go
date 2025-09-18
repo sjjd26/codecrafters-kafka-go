f46f40ae533d3e3c7078a192f385f5981b2687b4
Stuart Dingwell <stuartdingwell@gmail.com>
2025-09-19 00:26:21 +0100

    decoder methods
    Int8, Int16, Int32, Int64, UVarint, Varint, UUID, ByteArray

--- app/decoder.go
+++ app/decoder.go
@@ -1,21 +1,153 @@
 package main
 
-import "io"
+import (
+	"bufio"
+	"encoding/binary"
+	"fmt"
+	"io"
+)
 
-type SeekReader interface {
+type ByteReadReader interface {
 	io.Reader
-	io.Seeker
+	io.ByteReader
 }
 
 type Decoder struct {
-	reader SeekReader
+	reader ByteReadReader
 	pos    int
 	buf    []byte
 }
 
-func NewDecoder(r SeekReader) *Decoder {
+func NewDecoder(r io.Reader) *Decoder {
 	return &Decoder{
-		reader: r,
-		buf:    make([]byte, 4096),
+		reader: bufio.NewReader(r),
+		buf:    make([]byte, 1024),
 	}
+}
+
+func (d *Decoder) Int8() (int8, error) {
+	n, err := d.reader.Read(d.buf[:1])
+	if err != nil {
+		return 0, err
+	}
+	if n < 1 {
+		return 0, io.ErrUnexpectedEOF
+	}
+	d.pos += n
+	return int8(d.buf[0]), nil
+}
+
+func (d *Decoder) Int16() (int16, error) {
+	n, err := d.reader.Read(d.buf[:2])
+	if err != nil {
+		return 0, err
+	}
+	if n < 2 {
+		return 0, io.ErrUnexpectedEOF
+	}
+	d.pos += n
+	return int16(binary.BigEndian.Uint16(d.buf[:2])), nil
+}
+
+func (d *Decoder) Int32() (int32, error) {
+	n, err := d.reader.Read(d.buf[:4])
+	if err != nil {
+		return 0, err
+	}
+	if n < 4 {
+		return 0, io.ErrUnexpectedEOF
+	}
+	d.pos += n
+	return int32(binary.BigEndian.Uint32(d.buf[:4])), nil
+}
+
+func (d *Decoder) Int64() (int64, error) {
+	n, err := d.reader.Read(d.buf[:8])
+	if err != nil {
+		return 0, err
+	}
+	if n < 8 {
+		return 0, io.ErrUnexpectedEOF
+	}
+	d.pos += n
+	return int64(binary.BigEndian.Uint64(d.buf[:8])), nil
+}
+
+func (d *Decoder) UUID() (uuid, error) {
+	var u uuid
+	n, err := d.reader.Read(u[:])
+	if err != nil {
+		return uuid{}, err
+	}
+	if n < len(u) {
+		return uuid{}, io.ErrUnexpectedEOF
+	}
+	d.pos += n
+	return u, nil
+}
+
+func (d *Decoder) UVarint() (uint64, error) {
+	const (
+		continuationMask = 0x80
+		dataMask         = 0x7f
+		maxBytes         = 10
+	)
+
+	var shift uint
+	var b byte
+	var value uint64
+	for range maxBytes {
+		b, err := d.reader.ReadByte()
+		if err != nil {
+			return 0, err
+		}
+		d.pos++
+		// remove the continuation bit
+		part := uint64(b & dataMask)
+		// shift the
+		value |= part << shift
+
+		if b&continuationMask == 0 {
+			// TODO: overflow sanity check on final byte if i==9
+			return value, nil
+		}
+		shift += 7
+	}
+
+	if b&continuationMask != 0 {
+		return 0, fmt.Errorf("varint too long")
+	}
+	return 0, fmt.Errorf("incomplete varint")
+}
+
+func (d *Decoder) Varint() (int64, error) {
+	u, err := d.UVarint()
+	if err != nil {
+		return 0, err
+	}
+	// zigzag decode
+	return int64((u >> 1) ^ uint64(-(u & 1))), nil
+}
+
+func (d *Decoder) ByteArray() ([]byte, error) {
+	length, err := d.Int32()
+	length--
+	if err != nil {
+		return nil, err
+	}
+	if length <= 0 {
+		return nil, nil
+	}
+	if int(length) > len(d.buf) {
+		d.buf = make([]byte, length)
+	}
+	n, err := d.reader.Read(d.buf[:length])
+	if err != nil {
+		return nil, err
+	}
+	if n < int(length) {
+		return nil, io.ErrUnexpectedEOF
+	}
+	d.pos += n
+	return d.buf[:length], nil
 }
