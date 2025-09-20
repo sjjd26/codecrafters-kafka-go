8bf300eb17704d95c165152db7ab7680d0518eac
Stuart Dingwell <stuartdingwell@gmail.com>
2025-09-20 22:26:10 +0100

    switch to using a decoder for parsing of cluster metadata

--- app/clusterMetadata.go
+++ app/clusterMetadata.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"cmp"
-	"encoding/binary"
+	"errors"
 	"fmt"
+	"io"
+	"log"
 	"os"
 )
 
@@ -43,22 +44,22 @@ type TopicPartition struct {
 }
 
 type MetadataRecordBatch struct {
-	baseOffset           uint64
-	partitionLeaderEpoch uint32
-	magicByte            byte
-	CRC                  uint32
-	attributes           uint16
-	lastOffsetDelta      uint32
-	baseTimestamp        uint64
-	maxTimestamp         uint64
-	producerID           uint64
-	producerEpoch        uint16
-	baseSequence         uint32
+	baseOffset           int64
+	partitionLeaderEpoch int32
+	magicByte            int8
+	CRC                  int32
+	attributes           int16
+	lastOffsetDelta      int32
+	baseTimestamp        int64
+	maxTimestamp         int64
+	producerID           int64
+	producerEpoch        int16
+	baseSequence         int32
 	records              []any
 }
 
 type RecordBase struct {
-	attributes     byte
+	attributes     int8
 	timestampDelta int64
 	offsetDelta    int64
 	key            []byte
@@ -71,7 +72,7 @@ type RecordBase struct {
 type FeatureLevelRecord struct {
 	RecordBase
 	name         string
-	featureLevel uint16
+	featureLevel int16
 }
 
 type TopicRecord struct {
@@ -100,77 +101,99 @@ func retrieveClusterMetadata() ([]*MetadataRecordBatch, error) {
 		return nil, err
 	}
 	defer file.Close()
-	batches, err := parseClusterMetadataFile(*file)
+	batches, err := parseClusterMetadata(NewDecoder(file))
 	if err != nil {
 		return nil, err
 	}
 	return batches, nil
 }
 
-func parseClusterMetadataFile(file os.File) ([]*MetadataRecordBatch, error) {
-	readBuf := make([]byte, 1024)
-	n, err := file.Read(readBuf)
-	if err != nil {
-		return nil, fmt.Errorf("error reading metadata file: %w", err)
-	}
+func parseClusterMetadata(d *Decoder) ([]*MetadataRecordBatch, error) {
+	// readBuf := make([]byte, 1024)
+	// n, err := file.Read(readBuf)
+	// if err != nil {
+	// 	return nil, fmt.Errorf("error reading metadata file: %w", err)
+	// }
 
 	var batches []*MetadataRecordBatch
-	p := 0
-	for p < n {
-		batch, n, err := parseMetadataRecordBatch(readBuf[p:])
+	// p := 0
+	for {
+
+		batch, err := parseMetadataRecordBatch(d)
+		if errors.Is(err, io.EOF) {
+			log.Println("EOF reached")
+			break
+		}
 		if err != nil {
 			return nil, fmt.Errorf("error parsing metadata record batch: %w", err)
 		}
-		p += n
 		batches = append(batches, batch)
 	}
 
 	return batches, nil
 }
 
-func parseMetadataRecordBatch(data []byte) (*MetadataRecordBatch, int, error) {
-	if len(data) < 12 {
-		return nil, len(data), fmt.Errorf("not enough bytes to parse metadata record batch, need 12 got %d", len(data))
+func parseMetadataRecordBatch(d *Decoder) (*MetadataRecordBatch, error) {
+	baseOffset, err := d.Int64()
+	if err != nil {
+		return nil, err
 	}
-	p := 0
-	baseOffset := binary.BigEndian.Uint64(data[p : p+8])
-	p += 8
-	batchLength := int(binary.BigEndian.Uint32(data[p:p+4])) + 12
-	if batchLength > len(data) {
-		return nil, len(data), fmt.Errorf("declared metadata batch length %d is bigger than data length %d", batchLength, len(data))
+	// batch length, unused for now but needed to advance the reader
+	_, err = d.Int32()
+	if err != nil {
+		return nil, err
 	}
-	p += 4
-	partitionLeaderEpoch := binary.BigEndian.Uint32(data[p : p+4])
-	p += 4
-	magicByte := data[p]
-	p += 1
-	CRC := binary.BigEndian.Uint32(data[p : p+4])
-	p += 4
-	attributes := binary.BigEndian.Uint16(data[p : p+2])
-	p += 2
-	lastOffsetDelta := binary.BigEndian.Uint32(data[p : p+4])
-	p += 4
-	baseTimestamp := binary.BigEndian.Uint64(data[p : p+8])
-	p += 8
-	maxTimestamp := binary.BigEndian.Uint64(data[p : p+8])
-	p += 8
-	producerID := binary.BigEndian.Uint64(data[p : p+8])
-	p += 8
-	producerEpoch := binary.BigEndian.Uint16(data[p : p+2])
-	p += 2
-	baseSequence := binary.BigEndian.Uint32(data[p : p+4])
-	p += 4
-	numRecords := binary.BigEndian.Uint32(data[p : p+4])
-	p += 4
+	partitionLeaderEpoch, err := d.Int32()
+	if err != nil {
+		return nil, err
+	}
+	magicByte, err := d.Int8()
+	if err != nil {
+		return nil, err
+	}
+	CRC, err := d.Int32()
+	if err != nil {
+		return nil, err
+	}
+	attributes, err := d.Int16()
+	if err != nil {
+		return nil, err
+	}
+	lastOffsetDelta, err := d.Int32()
+	if err != nil {
+		return nil, err
+	}
+	baseTimestamp, err := d.Int64()
+	if err != nil {
+		return nil, err
+	}
+	maxTimestamp, err := d.Int64()
+	if err != nil {
+		return nil, err
+	}
+	producerID, err := d.Int64()
+	if err != nil {
+		return nil, err
+	}
+	producerEpoch, err := d.Int16()
+	if err != nil {
+		return nil, err
+	}
+	baseSequence, err := d.Int32()
+	if err != nil {
+		return nil, err
+	}
+	numRecords, err := d.Int32()
+	if err != nil {
+		return nil, err
+	}
 
 	records := make([]any, 0, numRecords)
-	// parse records in the batch
 	for i := 0; i < int(numRecords); i++ {
-		record, n, err := parseMetadataRecord(data[p:])
+		record, err := parseMetadataRecord(d)
 		if err != nil {
-			return nil, batchLength, fmt.Errorf("error parsing record %q: %w", data[p:], err)
+			return nil, fmt.Errorf("error parsing record: %w", err)
 		}
-		p += n
 		records = append(records, record)
 	}
 
@@ -188,56 +211,61 @@ func parseMetadataRecordBatch(data []byte) (*MetadataRecordBatch, int, error) {
 		baseSequence:         baseSequence,
 		records:              records,
 	}
-	return recordBatch, batchLength, nil
+	return recordBatch, nil
 }
 
-func parseMetadataRecord(data []byte) (any, int, error) {
-	p := 0
-	varInt, n, err1 := extractSignedVarInt(data[p:])
-	recordLength := int(varInt) + n
-	p += n
-	attributes := data[p]
-	p += 1
-	timestampDelta, n, err2 := extractSignedVarInt(data[p:])
-	p += n
-	offsetDelta, n, err3 := extractSignedVarInt(data[p:])
-	p += n
-	keyLength, n, err4 := extractSignedVarInt(data[p:])
-	p += n
-	var key []byte
-	if keyLength >= 0 {
-		key = data[p : p+int(keyLength)]
-		p += int(keyLength)
-	} else {
-		key = nil
-	}
-	// value length
-	varInt, n, err5 := extractSignedVarInt(data[p:])
-	valueLength := int(varInt)
-	p += n
-	if err := cmp.Or(err1, err2, err3, err4, err5); err != nil {
-		return nil, recordLength, err
-	}
-	// get headers before parsing the value (no headers for now so leave)
-	headersCount, n, err := extractVarInt(data[p+valueLength:])
-	p += n
+func parseMetadataRecord(d *Decoder) (any, error) {
+	// record length is not used directly, but need to advance the reader
+	_, err := d.Varint()
 	if err != nil {
-		return nil, recordLength, err
+		return nil, err
 	}
-	if headersCount > 0 {
-		return nil, recordLength, fmt.Errorf("headers not supported yet")
+	attributes, err := d.Int8()
+	if err != nil {
+		return nil, err
+	}
+	timestampDelta, err := d.Varint()
+	if err != nil {
+		return nil, err
+	}
+	offsetDelta, err := d.Varint()
+	if err != nil {
+		return nil, err
+	}
+	keyLength, err := d.Varint()
+	if err != nil {
+		return nil, err
+	}
+	var key []byte
+	if keyLength > 0 {
+		key, err = d.ByteArray(int(keyLength))
+		if err != nil {
+			return nil, err
+		}
+	}
+	// value length is not used directly, but need to advance the reader
+	_, err = d.Varint()
+	if err != nil {
+		return nil, err
 	}
 
-	// parse the value
-	frameVersion := int8(data[p])
-	p += 1
-	recordType := RecordType(data[p])
-	if recordType == RECORD_TYPE_UNKNOWN {
-		return nil, recordLength, fmt.Errorf("unknown record type %q", data[p])
+	// parse value
+	frameVersion, err := d.Int8()
+	if err != nil {
+		return nil, err
 	}
-	p += 1
-	version := int8(data[p])
-	p += 1
+	recordTypeByte, err := d.Int8()
+	if err != nil {
+		return nil, err
+	}
+	recordType := RecordType(recordTypeByte)
+	if recordType == RECORD_TYPE_UNKNOWN {
+		return nil, fmt.Errorf("unknown record type %q", recordTypeByte)
+	}
+	version, err := d.Int8()
+	if err != nil {
+		return nil, err
+	}
 
 	base := &RecordBase{
 		attributes:     attributes,
@@ -252,122 +280,127 @@ func parseMetadataRecord(data []byte) (any, int, error) {
 	var record any
 	switch recordType {
 	case RECORD_TYPE_FEATURE_LEVEL:
-		record, _, err = parseFeatureLevelRecord(data[p:], base)
+		record, err = parseFeatureLevelRecord(d, base)
 	case RECORD_TYPE_TOPIC:
-		record, _, err = parseTopicRecord(data[p:], base)
+		record, err = parseTopicRecord(d, base)
 	case RECORD_TYPE_PARTITION:
-		record, _, err = parsePartitionRecord(data[p:], base)
+		record, err = parsePartitionRecord(d, base)
 	default:
-		return nil, recordLength, fmt.Errorf("Record type not supported")
+		return nil, fmt.Errorf("Record type not supported")
 	}
-	return record, recordLength, err
+	if err != nil {
+		return nil, err
+	}
+
+	return record, nil
 }
 
-func parseFeatureLevelRecord(data []byte, base *RecordBase) (*FeatureLevelRecord, int, error) {
-	p := 0
-	nameLength, n, err := extractVarInt(data[p:])
-	p += n
+func parseFeatureLevelRecord(d *Decoder, base *RecordBase) (*FeatureLevelRecord, error) {
+	nameLength, err := d.Varint()
 	if err != nil {
-		return nil, p, err
+		return nil, err
 	}
-	name := string(data[p : p+int(nameLength)])
-	p += int(nameLength)
-	featureLevel := binary.BigEndian.Uint16(data[p : p+2])
-	p += 2
-
-	taggedFields, n, err := extractTaggedFields(data[p:])
-	p += n
+	nameBytes, err := d.ByteArray(int(nameLength))
 	if err != nil {
-		return nil, p, err
+		return nil, err
+	}
+	featureLevel, err := d.Int16()
+	if err != nil {
+		return nil, err
+	}
+	taggedFields, err := parseTaggedFields(d)
+	if err != nil {
+		return nil, err
 	}
 	base.taggedFields = taggedFields
 
 	featureLevelRecord := &FeatureLevelRecord{
 		RecordBase:   *base,
-		name:         name,
+		name:         string(nameBytes),
 		featureLevel: featureLevel,
 	}
-	return featureLevelRecord, p, nil
+	return featureLevelRecord, nil
 }
 
-func parseTopicRecord(data []byte, base *RecordBase) (*TopicRecord, int, error) {
-	p := 0
-	nameLength, n, err := extractVarInt(data[p:])
-	p += n
+func parseTopicRecord(d *Decoder, base *RecordBase) (*TopicRecord, error) {
+	nameLength, err := d.Varint()
 	if err != nil {
-		return nil, p, err
+		return nil, err
 	}
-	name := string(data[p : p+int(nameLength)])
-	p += int(nameLength)
-	var topicId uuid
-	copy(topicId[:], data[p:p+16])
-	p += 16
-
-	taggedFields, n, err := extractTaggedFields(data[p:])
-	p += n
+	nameBytes, err := d.ByteArray(int(nameLength))
 	if err != nil {
-		return nil, p, err
+		return nil, err
+	}
+	topicId, err := d.UUID()
+	if err != nil {
+		return nil, err
+	}
+	taggedFields, err := parseTaggedFields(d)
+	if err != nil {
+		return nil, err
 	}
 	base.taggedFields = taggedFields
 
 	record := &TopicRecord{
 		RecordBase: *base,
-		name:       name,
+		name:       string(nameBytes),
 		topicId:    topicId,
 	}
-	return record, p, nil
+	return record, nil
 }
 
-func parsePartitionRecord(data []byte, base *RecordBase) (*PartitionRecord, int, error) {
-	p := 0
-	partitionId := int32(binary.BigEndian.Uint32(data[p : p+4]))
-	p += 4
-	var topicId uuid
-	copy(topicId[:], data[p:p+16])
-	p += 16
-	// replicas
-	replicas, n, err := extractReplicaList(data[p:])
-	p += n
+func parsePartitionRecord(d *Decoder, base *RecordBase) (*PartitionRecord, error) {
+	partitionId, err := d.Int32()
 	if err != nil {
-		return nil, p, err
+		return nil, err
 	}
-	inSyncReplicas, n, err := extractReplicaList(data[p:])
-	p += n
+	topicId, err := d.UUID()
 	if err != nil {
-		return nil, p, err
+		return nil, err
 	}
-	removingReplicas, n, err := extractReplicaList(data[p:])
-	p += n
+	replicas, err := parseReplicaArray(d)
 	if err != nil {
-		return nil, p, err
+		return nil, err
 	}
-	addingReplicas, n, err := extractReplicaList(data[p:])
-	p += n
+	inSyncReplicas, err := parseReplicaArray(d)
 	if err != nil {
-		return nil, p, err
+		return nil, err
 	}
-	leaderReplica := int32(binary.BigEndian.Uint32(data[p : p+4]))
-	p += 4
-	leaderEpoch := int32(binary.BigEndian.Uint32(data[p : p+4]))
-	p += 4
-	partitionEpoch := int32(binary.BigEndian.Uint32(data[p : p+4]))
-	p += 4
-	numDirectories, n, err := extractVarInt(data[p:])
-	p += n
+	removingReplicas, err := parseReplicaArray(d)
 	if err != nil {
-		return nil, p, err
+		return nil, err
+	}
+	addingReplicas, err := parseReplicaArray(d)
+	if err != nil {
+		return nil, err
+	}
+	leaderReplica, err := d.Int32()
+	if err != nil {
+		return nil, err
+	}
+	leaderEpoch, err := d.Int32()
+	if err != nil {
+		return nil, err
+	}
+	partitionEpoch, err := d.Int32()
+	if err != nil {
+		return nil, err
+	}
+	numDirectories, err := d.Varint()
+	if err != nil {
+		return nil, err
 	}
 	directories := make([]uuid, 0, numDirectories)
 	for range numDirectories {
-		var dirID uuid
-		copy(dirID[:], data[p:p+16])
-		p += 16
-		directories = append(directories, dirID)
+		dirId, err := d.UUID()
+		if err != nil {
+			return nil, err
+		}
+		directories = append(directories, dirId)
 	}
-	taggedFields, n, err := extractTaggedFields(data[p:])
-	p += n
+	taggedFields, err := parseTaggedFields(d)
 	if err != nil {
-		return nil, p, err
+		return nil, err
 	}
 	base.taggedFields = taggedFields
 
@@ -384,42 +417,35 @@ func parsePartitionRecord(data []byte, base *RecordBase) (*PartitionRecord, int,
 		partitionEpoch:   partitionEpoch,
 		directories:      directories,
 	}
-	return partitionRecord, p, nil
+	return partitionRecord, nil
 }
 
-func extractTaggedFields(data []byte) ([]byte, int, error) {
-	p := 0
-	numTaggedFields, n, err := extractVarInt(data[p:])
-	p += n
+func parseTaggedFields(d *Decoder) ([]byte, error) {
+	numTaggedFields, err := d.Varint()
 	if err != nil {
-		return nil, p, fmt.Errorf("Failed to extract tagged fields: %w", err)
+		return nil, fmt.Errorf("error parsing tagged fields: %w", err)
 	}
 	if numTaggedFields > 0 {
-		return nil, p, fmt.Errorf("Tagged fields not supported yet")
+		return nil, fmt.Errorf("tagged fields not supported yet")
 	}
-	return nil, p, nil
+	return nil, nil
 }
 
-// extractReplicaList produces a slice of int32 replica ids from an input byte slice
-// as well as the number of bytes consumed/read
-func extractReplicaList(data []byte) ([]int32, int, error) {
-	p := 0
-	numReplicas, n, err := extractVarInt(data[p:])
-	p += n
+// parseReplicaArray produces a slice of int32 replica ids from the decoder
+func parseReplicaArray(d *Decoder) ([]int32, error) {
+	numReplicas, err := d.Varint()
 	if err != nil {
-		return nil, p, fmt.Errorf("error getting the list length: %w", err)
-	}
-	minBytes := int(numReplicas)*4 + p
-	if minBytes > len(data) {
-		return nil, len(data), fmt.Errorf("not enough bytes according to length %v, got %v, required %v", numReplicas, len(data), minBytes)
+		return nil, err
 	}
 	replicas := make([]int32, 0, numReplicas)
 	for range numReplicas {
-		replica := int32(binary.BigEndian.Uint32(data[p : p+4]))
-		p += 4
+		replica, err := d.Int32()
+		if err != nil {
+			return nil, err
+		}
 		replicas = append(replicas, replica)
 	}
-	return replicas, p, nil
+	return replicas, nil
 }
 
 func extractVarInt(data []byte) (value uint64, consumed int, err error) {
@@ -432,7 +458,7 @@ func extractVarInt(data []byte) (value uint64, consumed int, err error) {
 	var shift uint
 	for i, b := range data {
 		if i == maxBytes {
-			return 0, i, fmt.Errorf("Varint too long")
+			return 0, i, fmt.Errorf("varint too long")
 		}
 		// remove the continuation bit
 		part := uint64(b & dataMask)
@@ -447,7 +473,7 @@ func extractVarInt(data []byte) (value uint64, consumed int, err error) {
 		shift += 7
 	}
 
-	return 0, consumed, fmt.Errorf("Incomplete varint")
+	return 0, consumed, fmt.Errorf("incomplete varint")
 }
 
 func extractSignedVarInt(data []byte) (value int64, consumed int, err error) {
