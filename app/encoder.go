e06c5e5828d32a0dec72f1cd59b43cb9ded3387a
Stuart Dingwell <stuartdingwell@gmail.com>
2025-09-24 22:32:06 +0100

    switch to a buffer-backed encoder

--- app/encoder.go
+++ app/encoder.go
@@ -1,40 +1,49 @@
 package main
 
 import (
-	"bufio"
-	"io"
+	"encoding/binary"
 )
 
-type ByteWriteWriter interface {
-	io.Writer
-	io.ByteWriter
-}
-
 type Encoder struct {
-	writer  ByteWriteWriter
 	scratch []byte
 	err     error
+	buf     []byte
 }
 
-func NewEncoder(w io.Writer) *Encoder {
+func NewEncoder(capacity int) *Encoder {
 	return &Encoder{
-		writer:  bufio.NewWriter(w),
 		scratch: make([]byte, 8),
+		buf:     make([]byte, 0, capacity),
 	}
+}
+
+func (e *Encoder) Bytes() []byte { return e.buf }
+func (e *Encoder) Reset()        { e.buf = e.buf[:0] }
+
+func (e *Encoder) WriteBytes(b []byte) {
+	e.buf = append(e.buf, b...)
 }
 
 func (e *Encoder) Int8(n int8) {
-	if e.err != nil {
-		return
-	}
-	_, e.err = e.writer.Write([]byte{byte(n)})
+	e.buf = append(e.buf, byte(n))
 }
 
 func (e *Encoder) Int16(n int16) {
-	if e.err != nil {
-		return
-	}
-	e.scratch[0] = byte(n)
-	e.scratch[1] = byte(n >> 8)
-	_, e.err = e.writer.Write(e.scratch[:2])
+	e.buf = binary.BigEndian.AppendUint16(e.buf, uint16(n))
+}
+
+func (e *Encoder) Int32(n int32) {
+	e.buf = binary.BigEndian.AppendUint32(e.buf, uint32(n))
+}
+
+func (e *Encoder) Int64(n int64) {
+	e.buf = binary.BigEndian.AppendUint64(e.buf, uint64(n))
+}
+
+func (e *Encoder) UVarint(v uint64) {
+	e.buf = binary.AppendUvarint(e.buf, v)
+}
+
+func (e *Encoder) UUID(u uuid) {
+	e.buf = append(e.buf, u[:]...)
 }
